0fae4cccf76f446ac005a4d4ce8067113c0c3fef
Roman <ar>
2016-02-28 12:29:54 +0600

    28.02

--- js/src/script.js
+++ js/src/script.js
@@ -1,3 +1,4 @@
+
 var perA = prompt("Для решения этого уравнениея введите значение \"а\"", 0);
 var perB = prompt("Введите значение \"b\"", 0);
 var perC = prompt("Введите значение \"c\"", 0);
@@ -6,12 +7,18 @@ var discrimenant = (perB * perB) - 4 * perA * perC;
 if (discrimenant > 0) {
     var x1 = ((-perB) + Math.sqrt(discrimenant)) / 2 * perA;
     var x2 = ((-perB) - Math.sqrt(discrimenant)) / 2 * perA;
-    alert("Х1= " + x1 + "\nХ2= " + x2);
+    console.log("Х1= " + x1 + "\nХ2= " + x2);
 } else if (discrimenant == 0) {
     var x = (-perB) / 2 * perA;
-    alert("Корень Х равен: " + x);
+    console.log("Корень Х равен: " + x);
 } else {
-    alert("Нет решения уравнения");
+    console.log("Нет решения уравнения");
 }
 
 
+
+/*var list = [2,6,8,7,9,8,22,11,44,55,9,5,477,1,5446];
+list.sort(function(e1,e2){
+    return e2-e1;
+});
+alert(list);*/
